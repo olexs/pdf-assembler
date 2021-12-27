f88b5e22ef107e6e2033684885d96acbb844da0a
Olexandr Savchuk <mail@olex.biz>
2021-12-27 22:19:37 +0100

    Partially fixed live reload

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,10 +1,51 @@
+const webpack = require('webpack');
+
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  },
+  { test: /\.afm$/, type: 'asset/source' },
+  {
+    test: /src[/\\]static-assets/,
+    type: 'asset/inline',
+    generator: {
+      dataUrl: content => {
+        return content.toString('base64');
+      }
+    }
+  },
+  {
+    test: /src[/\\]lazy-assets/,
+    type: 'asset/resource'
+  },
+  {
+    //enforce: 'post',
+    test: /unicode-properties[/\\]index.js$/,
+    loader: 'transform-loader',
+    options: {
+      brfs: {}
+    }
+  },
+  {
+    //enforce: 'post',
+    test: /fontkit[/\\]index.js$/,
+    loader: 'transform-loader',
+    options: {
+      brfs: {}
+    }
+  },
+  {
+    //enforce: 'post',
+    test: /linebreak[/\\]src[/\\]linebreaker.js/,
+    loader: 'transform-loader',
+    options: {
+      brfs: {}
+    }
+  });
 
 module.exports = {
   module: {
@@ -14,7 +55,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
     alias: {
-			fs: 'pdfkit/js/virtual-fs.js' // hot reload fix, src: https://github.com/blikblum/pdfkit-webpack-example/blob/master/webpack.config.js
-		}
+      fs: 'pdfkit/js/virtual-fs.js'
+    }
   },
 };
